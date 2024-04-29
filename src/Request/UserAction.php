d1e52b43f4d16ec4477b6f6425824e65f576e20a
Kamshory, MT <kamshory@yahoo.com>
2024-04-29 10:16:52 +0700

    Update UserAction.php

--- src/Request/UserAction.php
+++ src/Request/UserAction.php
@@ -7,6 +7,8 @@ class UserAction
     const INSERT = "insert";
     const UPDATE = "update";
     const DELETE = "delete";
+    const ACTIVATE = "activate";
+    const DEACTIVATE = "deactivate";
     const APPROVE = "approve";
     const REJECT = "reject";
     const SPECIAL_ACTION = "special_action";
