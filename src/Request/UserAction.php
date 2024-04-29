75add9a7935c306febd5fc248d6f8cb9ab2711a4
Kamshory, MT <kamshory@yahoo.com>
2024-04-29 10:09:25 +0700

    Create UserAction.php

---
+++ src/Request/UserAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MagicObject\Request;
+
+class UserAction
+{
+    const INSERT = "insert";
+    const UPDATE = "update";
+    const DELETE = "delete";
+    const APPROVE = "approve";
+    const REJECT = "reject";
+    const SPECIAL_ACTION = "special_action";
+}
