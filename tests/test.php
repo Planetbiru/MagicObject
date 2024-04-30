6f8d6d9db24e9990c63d91a665a9a25441a418ee
Kamshory, MT <kamshory@yahoo.com>
2024-04-30 12:58:42 +0700

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,9 @@
 <?php
 
-use MagicObject\Util\AttrUtil;
+use MagicObject\Util\PicoTestValueUtil;
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 
-echo (new AttrUtil())->doReturnAttributeSelected()->whenEquals(1, 1);
+echo (new PicoTestValueUtil())->doReturnAttributeChecked()->whenEquals(1, 1);
 
