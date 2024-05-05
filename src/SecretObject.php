b3177f30e648362c1772e1b7d629aa18b7d927b6
Kamshory, MT <kamshory@yahoo.com>
2024-05-05 12:14:09 +0700

    Update SecretObject.php

--- src/SecretObject.php
+++ src/SecretObject.php
@@ -901,6 +901,17 @@ class SecretObject extends stdClass //NOSONAR
     {
         return !$this->_snakeJson();
     }
+
+    /**
+     * Chek if magic object is empty
+     *
+     * @return boolean
+     */
+    public function empty()
+    {
+        $keys = array_keys($this->valueArray());
+        return empty($keys);
+    }
     
     /**
      * Property list
