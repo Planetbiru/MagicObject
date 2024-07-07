a825c355d47d1162cc2734b21dddfa83023d3e80
Kamshory, MT <kamshory@yahoo.com>
2024-07-07 22:35:41 +0700

    Update PicoPageControl.php

--- src/Database/PicoPageControl.php
+++ src/Database/PicoPageControl.php
@@ -110,14 +110,12 @@ class PicoPageControl
 
         if(isset($this->first) && $this->pageData->getPageNumber() > 2)
         {
-            // Old code $lines[] = '<span class="page-selector" data-page-number="1"><a href="'.PicoPagination::getPageUrl(1, $this->parameterName, $this->path).'">'.$this->first.'</a></span>';
             $lines[] = sprintf($format, '', 1, PicoPagination::getPageUrl(1, $this->parameterName, $this->path), $this->first);
         }
 
         if(isset($this->prev) && $this->pageData->getPageNumber() > 1)
         {
             $prevPg = $this->pageData->getPageNumber() - 1;
-            // Old code $lines[] = '<span class="page-selector" data-page-number="$prevPg"><a href="'.PicoPagination::getPageUrl($prevPg, $this->parameterName, $this->path).'">'.$this->prev.'</a></span>';
             $lines[] = sprintf($format, '', $prevPg, PicoPagination::getPageUrl($prevPg, $this->parameterName, $this->path), $this->prev);
         }
 
@@ -125,21 +123,18 @@ class PicoPageControl
         {
             $lastNavPg = $pg['page'];
             $selected = $pg['selected'] ? ' page-selected' : '';
-            // old code $lines[] = '<span class="page-selector'.$selected.'" data-page-number="'.$pg['page'].'"><a href="'.PicoPagination::getPageUrl($pg['page'], $this->parameterName, $this->path).'">'.$pg['page'].'</a></span>';
             $lines[] = sprintf($format, $selected, $lastNavPg, PicoPagination::getPageUrl($lastNavPg, $this->parameterName, $this->path), $lastNavPg);
         }
 
-        if(isset($this->next) && $this->pageData->getPageNumber() < ($this->pageData->getTotalPage() - 1))
+        if(isset($this->next) && $this->pageData->getPageNumber() < ($this->pageData->getTotalPage()))
         {
             $nextPg = $this->pageData->getPageNumber() + 1;
-            // Old code $lines[] = '<span class="page-selector" data-page-number="$nextPg"><a href="'.PicoPagination::getPageUrl($nextPg, $this->parameterName, $this->path).'">'.$this->next.'</a></span>';
             $lines[] = sprintf($format, '', $nextPg, PicoPagination::getPageUrl($nextPg, $this->parameterName, $this->path), $this->next);
         }
 
-        if(isset($this->last) && $lastNavPg < ($this->pageData->getTotalPage() - 2))
+        if(isset($this->last) && $this->pageData->getPageNumber() < ($this->pageData->getTotalPage() - 1))
         {
             $lastPg = $this->pageData->getTotalPage();
-            // Old code $lines[] = '<span class="page-selector" data-page-number="$lastPg"><a href="'.PicoPagination::getPageUrl($lastPg, $this->parameterName, $this->path).'">'.$this->last.'</a></span>';
             $lines[] = sprintf($format, '', $lastPg, PicoPagination::getPageUrl($lastPg, $this->parameterName, $this->path), $this->last);
         }
 
