f8d97237516a139326cb0afb490124d0dd83c62a
Kamshory, MT <kamshory@alto.id>
2024-07-15 14:53:58 +0700

    Update PicoPredicate.php

--- src/Database/PicoPredicate.php
+++ src/Database/PicoPredicate.php
@@ -31,7 +31,7 @@ class PicoPredicate //NOSONAR
     private $filterLogic = null;
 
     /**
-     * Constructor. If $field given, it will call equals method
+     * Constructor. If $field given, it will call in method for array and equals for others
      *
      * @param string $field
      * @param mixed $value
