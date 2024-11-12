bcfab69efccd413087792cf242bc3d3e993b8e07
Kamshory, MT <kamshory@yahoo.com>
2024-11-13 06:02:27 +0700

    Update SecretObject.php

--- src/SecretObject.php
+++ src/SecretObject.php
@@ -129,17 +129,19 @@ class SecretObject extends stdClass //NOSONAR
     }
 
     /**
-     * Processes object information to determine encryption and decryption requirements.
+     * Analyzes the class's parameters and properties to determine which should be 
+     * encrypted or decrypted based on annotations.
      *
-     * This method retrieves the class parameters and properties using reflection, 
-     * parsing annotations to identify which properties should be encrypted or 
-     * decrypted. It populates the respective lists of properties based on the 
-     * annotations found.
+     * This method uses reflection to retrieve the class's parameters and properties. 
+     * It then parses annotations associated with these members to identify which 
+     * properties should undergo encryption or decryption during specific stages 
+     * (before storage or before retrieval). The appropriate lists of properties 
+     * are populated accordingly.
      *
      * @return void
      *
      * @throws InvalidAnnotationException If an invalid annotation is encountered 
-     *                                    while processing class parameters.
+     *                                    while processing class parameters or properties.
      */
     private function _objectInfo()
     {
@@ -148,42 +150,34 @@ class SecretObject extends stdClass //NOSONAR
         $params = $reflexClass->getParameters();
         $props = $reflexClass->getProperties();
 
-        foreach($params as $paramName=>$paramValue)
-        {
-            try
-            {
+        // Process each class parameter
+        foreach ($params as $paramName => $paramValue) {
+            try {
                 $vals = $reflexClass->parseKeyValue($paramValue);
                 $this->_classParams[$paramName] = $vals;
-            }
-            catch(InvalidQueryInputException $e)
-            {
-                throw new InvalidAnnotationException("Invalid annotation @".$paramName);
+            } catch (InvalidQueryInputException $e) {
+                throw new InvalidAnnotationException("Invalid annotation @" . $paramName);
             }
         }
 
-        // iterate each properties of the class
-        foreach($props as $prop)
-        {
+        // Process each class property
+        foreach ($props as $prop) {
             $reflexProp = new PicoAnnotationParser($className, $prop->name, 'property');
             $parameters = $reflexProp->getParameters();
 
-            // add property list to be encryped or decrypted
-            foreach($parameters as $param=>$val)
-            {
-                if(strcasecmp($param, self::ANNOTATION_ENCRYPT_IN) == 0)
-                {
+            // Check each property for encryption/decryption annotations
+            foreach ($parameters as $param => $val) {
+                if (strcasecmp($param, self::ANNOTATION_ENCRYPT_IN) == 0) {
+                    // Property should be encrypted before storing
                     $this->_encryptInProperties[] = $prop->name;
-                }
-                else if(strcasecmp($param, self::ANNOTATION_DECRYPT_OUT) == 0)
-                {
+                } else if (strcasecmp($param, self::ANNOTATION_DECRYPT_OUT) == 0) {
+                    // Property should be decrypted before retrieval
                     $this->_decryptOutProperties[] = $prop->name;
-                }
-                else if(strcasecmp($param, self::ANNOTATION_ENCRYPT_OUT) == 0)
-                {
+                } else if (strcasecmp($param, self::ANNOTATION_ENCRYPT_OUT) == 0) {
+                    // Property should be encrypted before retrieval
                     $this->_encryptOutProperties[] = $prop->name;
-                }
-                else if(strcasecmp($param, self::ANNOTATION_DECRYPT_IN) == 0)
-                {
+                } else if (strcasecmp($param, self::ANNOTATION_DECRYPT_IN) == 0) {
+                    // Property should be decrypted before storing
                     $this->_decryptInProperties[] = $prop->name;
                 }
             }
