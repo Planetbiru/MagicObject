ecbcf707c9483205a64bb8a017ee8d87ec959974
Kamshory, MT <kamshory@yahoo.com>
2025-01-11 23:26:17 +0700

    Update docblock

--- src/Util/PicoParsedown.php
+++ src/Util/PicoParsedown.php
@@ -2,18 +2,18 @@
 
 namespace MagicObject\Util;
 
-#
-#
-# Parsedown
-# http://parsedown.org
-#
-# (c) Emanuil Rusev
-# http://erusev.com
-#
-# For the full license information, view the LICENSE file that was distributed
-# with this source code.
-#
-#
+/**
+ *
+ * Parsedown
+ * http://parsedown.org
+ *
+ * (c) Emanuil Rusev
+ * http://erusev.com
+ *
+ * For the full license information, view the LICENSE file that was distributed
+ * with this source code.
+ *
+ */
 
 class PicoParsedown // NOSONAR
 {
