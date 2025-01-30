6a4e6529f460f02849613000b6a48e1802d090f4
Kamshory, MT <kamshory@alto.id>
2025-01-30 15:01:06 +0700

    Update fix default value

--- src/Util/Database/PicoDatabaseUtilPostgreSql.php
+++ src/Util/Database/PicoDatabaseUtilPostgreSql.php
@@ -315,7 +315,7 @@ class PicoDatabaseUtilPostgreSql extends PicoDatabaseUtilBase implements PicoDat
         $result = $defaultValue;
         if(self::isTypeBoolean($type))
         {
-            $result = ($defaultValue != 0 || strtolower($defaultValue) == 'true') ? 'true' : 'false';
+            $result = ($defaultValue != 0 || strtolower($defaultValue) == 'true') ? 'TRUE' : 'FALSE';
         }
         else if(self::isNativeValue($defaultValue)) {
             $result = $defaultValue;
