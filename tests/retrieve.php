a647dba630a8780ec35ceaf6b94fe24f96ada212
Kamshory, MT <kamshory@alto.id>
2025-04-16 23:33:40 +0700

    Add method mergeWith

--- tests/retrieve.php
+++ tests/retrieve.php
@@ -16,4 +16,39 @@ prop1:
 
 $object->loadYamlString($yaml, false, true, true);
 
-echo $object->retrieve('prop1', 'any');
+$object2 = new MagicObject();
+
+$yaml = '
+prop11:
+  prop12:
+    prop13:
+      prop14: Ok
+';
+
+$object2->loadYamlString($yaml, false, true, true);
+
+$object->mergeWith($object2);
+
+//echo $object;
+
+
+$obj1 = new MagicObject();
+$obj1->loadYamlString('
+user:
+  name: ALice
+client:
+  address: Jakarta
+', false, true, true);
+
+$obj2 = new MagicObject();
+$obj2->loadYamlString('
+user:
+  email: alice@example.com
+client:
+  name: Ana
+', false, true, true);
+
+
+$obj1->mergeWith($obj2);
+
+echo $obj1;
